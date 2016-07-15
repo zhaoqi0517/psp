8ac0900bb0b77744cd766ab29ff59bf1a53164c5
Qi Zhao <zhaoqi0517@gmail.com>
2016-07-16 00:10:39 +0800

    Initial commit

---
+++ psp-core/src/main/java/com/zhaoqi/psp/App.java
@@ -0,0 +1,13 @@
+package com.zhaoqi.psp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
